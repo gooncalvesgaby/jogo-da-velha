6d728be7863b08f5d12db2b87cc020ef05276578
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-04-25 23:01:45 -0300

    alteração no design

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-let fields = document.querySelectorAll(".box");
-function Adicionar() {
-    console.log(fields)
-    
+const celulas = document.querySelectorAll(".box");
+let checarTurno = true;
+const jogador_o = "O";
+const jogador_x = "X";
+
+document.addEventListener("click", (event) => {
+    if(event.target.matches(".celula")) {
+        jogar(event.target.id);
+    }
+});
+
+function jogar(id) {
+    const celula = document.getElementById(id);
+    turno = checarTurno ? jogador_o : jogador_x;
+    celula.textContent = turno;
+    checarTurno = !checarTurno;
+    checarVencedor(turno);
 }
+
+function checarVencedor(turno) {
+
+};
