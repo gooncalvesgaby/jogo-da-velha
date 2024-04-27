0b69a6ade270508de895889822c0319d1025956e
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-04-27 00:59:48 -0300

    validacoes

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const celulas = document.querySelectorAll(".box");
+const celulas = document.querySelectorAll(".celula");
 let checarTurno = true;
-const jogador_o = "O";
+
 const jogador_x = "X";
+const jogador_o = "O";
+
+const combinacoes = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
 document.addEventListener("click", (event) => {
     if(event.target.matches(".celula")) {
@@ -11,12 +23,51 @@ document.addEventListener("click", (event) => {
 
 function jogar(id) {
     const celula = document.getElementById(id);
-    turno = checarTurno ? jogador_o : jogador_x;
+    turno = checarTurno ? jogador_x : jogador_o;
     celula.textContent = turno;
-    checarTurno = !checarTurno;
+    celula.classList.add(turno);
     checarVencedor(turno);
 }
 
 function checarVencedor(turno) {
+    const vencedor = combinacoes.some((comb) => {
+        return comb.every((index) => {
+            return celulas[index].classList.contains(turno);
+        })
+    });
 
-};
+    if (vencedor){
+        encerrarJogo(turno);
+    } else if (checarEmpate()) {
+        encerrarJogo();
+    } else {
+        checarTurno = !checarTurno;
+    }
+}
+
+function checarEmpate() {
+    let x = 0;
+    let o = 0;
+
+    for (index in celulas) {
+        if(!isNaN(index)) {
+            if(celulas[index].classList.contains(jogador_x)) {
+                x++;
+            }
+    
+            if(celulas[index].classList.contains(jogador_o)) {
+                o++;
+            }
+        }
+    }
+
+        return x + o === 9 ? true : false;
+}
+
+function encerrarJogo(vencedor = null) {
+    if (vencedor) {
+        console.log("vencedor: " + vencedor);
+    } else {
+        console.log("Empatou");
+    }
+}
